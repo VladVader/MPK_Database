91f2cea8ff0e9af6cc9a1f08f0df544be94bb64b
romik9man@gmail.com <9379992a>
2017-12-27 14:21:50 +0100

    entities add

---
+++ src/main/java/Rozchod.java
@@ -0,0 +1,100 @@
+public class Rozchod {
+    @PrimaryKey
+    @Persistent(valueStrategy = IdGeneratorStrategy.IDENTITY)
+    private Key key;
+
+    @Persistent
+    private String tytul;
+
+    @Persistent
+    private int kwota;
+
+    @Persistent
+    private int kwartal;
+
+    public Rozchod(String tytul, int kwota, int kwartal, int rok, Bilans bilans) {
+        this.tytul = tytul;
+        this.kwota = kwota;
+        this.kwartal = kwartal;
+        this.rok = rok;
+        this.bilans = bilans;
+    }
+
+    @Persistent
+    private int rok;
+
+    public String getTytul() {
+        return tytul;
+    }
+
+    public void setTytul(String tytul) {
+        this.tytul = tytul;
+    }
+
+    public int getKwota() {
+        return kwota;
+    }
+
+    public void setKwota(int kwota) {
+        this.kwota = kwota;
+    }
+
+    public int getKwartal() {
+        return kwartal;
+    }
+
+    public void setKwartal(int kwartal) {
+        this.kwartal = kwartal;
+    }
+
+    public int getRok() {
+        return rok;
+    }
+
+    public void setRok(int rok) {
+        this.rok = rok;
+    }
+
+    public Bilans getBilans() {
+        return bilans;
+    }
+
+    public void setBilans(Bilans bilans) {
+        this.bilans = bilans;
+    }
+
+    @Persistent
+    private Bilans bilans;
+
+    public getAllNaprawy(){
+        return pm.getObjects(Naprawa.class)
+    }
+
+    public getAllPensje(){
+        return pm.getObjects(Pensja.class)
+    }
+
+    public getAllUrlopy(){
+        return pm.getObjects(Urlop.class)
+    }
+
+    public obliczKwote(){
+        int kwota=0;
+        for (pm.getObjects(Naprawa.class):naprawa
+             ) {
+            kwota+=naprawa.getKwota();
+        }
+
+        for (pm.getObjects(Pensja.class):pensja
+             ) {
+            kwota+=pensja.getKwota();
+        }
+
+        for (pm.getObjects(Urlop.class):urlop
+             ) {
+            kwota+=urlop.getKwota();
+        }
+    }
+
+    return kwota;
+}
---
+++ src/main/java/Transport.java
@@ -0,0 +1,95 @@
+public class Transport {
+    @PrimaryKey
+    @Persistent(valueStrategy = IdGeneratorStrategy.IDENTITY)
+    private Key key;
+
+    @Persistent
+    private Int numer;
+
+    public Transport(Int numer, Data rokProdukcji, String producent, Int liczbaMiejsc, String typ, Zajezdnia zajezdnia) {
+        this.numer = numer;
+        this.rokProdukcji = rokProdukcji;
+        this.producent = producent;
+        this.liczbaMiejsc = liczbaMiejsc;
+        this.typ = typ;
+        this.zajezdnia = zajezdnia;
+    }
+
+    @Persistent
+    private Data rokProdukcji;
+
+    @Persistent
+    private String producent;
+
+    public Int getNumer() {
+        return numer;
+    }
+
+    public void setNumer(Int numer) {
+        this.numer = numer;
+    }
+
+    public Data getRokProdukcji() {
+        return rokProdukcji;
+    }
+
+    public void setRokProdukcji(Data rokProdukcji) {
+        this.rokProdukcji = rokProdukcji;
+    }
+
+    public String getProducent() {
+        return producent;
+    }
+
+    public void setProducent(String producent) {
+        this.producent = producent;
+    }
+
+    public Int getLiczbaMiejsc() {
+        return liczbaMiejsc;
+    }
+
+    public void setLiczbaMiejsc(Int liczbaMiejsc) {
+        this.liczbaMiejsc = liczbaMiejsc;
+    }
+
+    public String getTyp() {
+        return typ;
+    }
+
+    public void setTyp(String typ) {
+        this.typ = typ;
+    }
+
+    public Zajezdnia getZajezdnia() {
+        return zajezdnia;
+    }
+
+    public void setZajezdnia(Zajezdnia zajezdnia) {
+        this.zajezdnia = zajezdnia;
+    }
+
+    @Persistent
+    private Int liczbaMiejsc;
+
+    @Persistent
+    private String typ;
+
+    @Persistent
+    private Zajezdnia zajezdnia;
+
+    public Transport searchTransport(Int numer){
+        for ((pm.getObjects(Transport.class):transport)
+             ) {
+            if(transport.getNumer()==numer) {
+                return transport;
+            }
+        }
+    }
+
+
+    public Boolean deleteTransport(Transport transport){
+        pm.deletePersistent(transport);
+    }
+
+}
